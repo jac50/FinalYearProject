ed7116e7054ee05c7918c149ffd7c6161ddf6335
James Cox <james.a.cox91@googlemail.com>
2014-02-09 12:22:36 +0000

    Version 1 of TDEV

--- PacketMetrics/TDEV.r
+++ PacketMetrics/TDEV.r
@@ -3,20 +3,20 @@
 
 #Input Data 
 
-TDEV <- function(nTo,N){
+TDEV <- function(nTo,N,x){
 To <- 0.1
 n <- nTo / To
-test = c(1,2,3)
+#generate test data
+x <- seq(0,1000)
 
 outerStep <- 0
 for (j in 1:(N-3*n + 1)){
 	interimStep <- 0
 	for (i in j:(n+j - 1)){
-		interimStep <- interimStep + x(n -1) + x(n) + x(n+1) #Need to change
+		interimStep <- interimStep + x[i + 2*n] - 2* x[i+n] + x[i] 
 	}
 	interimStep = interimStep ^ 2
 	outerStep = outerStep + interimStep
-#	print("Hello")
 }
 outerStep = outerStep / (6 * n^2 * (N - 3*n + 1));
 result <- sqrt(6)
