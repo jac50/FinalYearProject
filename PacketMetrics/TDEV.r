65e9736dc7d782a3646a37190b8ae54364f0f36d
James Cox <james.a.cox91@googlemail.com>
2014-02-10 09:15:54 +0000

    Completed Script. Calculating a centred mean around the subscript

--- PacketMetrics/TDEV.r
+++ PacketMetrics/TDEV.r
@@ -1,5 +1,8 @@
 #!/usr/bin/env Rscript
-
+# ---- Script to Calculate the TDEV of a set of input data. 
+# ---- Uses the built in Mean Function. 
+# ---- No plotting is done in this file
+# ---- 
 
 #Input Data 
 
@@ -8,18 +11,20 @@ To <- 0.1
 n <- nTo / To
 #generate test data
 x <- seq(0,1000)
+window <- 15
+windowSide <- (window - 1) / 2
 
 outerStep <- 0
 for (j in 1:(N-3*n + 1)){
 	interimStep <- 0
 	for (i in j:(n+j - 1)){
-		interimStep <- interimStep + x[i + 2*n] - 2* x[i+n] + x[i] 
+		interimStep <- interimStep + mean(x[i + 2*n - windowSide:i + 2*n + windowSide]) - 2* x[i+n - windowSide : i + n + windowSide] + x[i - windowSide : i + windowSide] 
 	}
 	interimStep = interimStep ^ 2
 	outerStep = outerStep + interimStep
 }
 outerStep = outerStep / (6 * n^2 * (N - 3*n + 1));
-result <- sqrt(6)
+result <- sqrt(outerStep)
 return(result) 
 }
 
