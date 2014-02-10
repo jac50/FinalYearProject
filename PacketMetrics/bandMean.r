7d4adc863c078333b42822c64deb996d9c9cede1
James Cox <james.a.cox91@googlemail.com>
2014-02-10 13:24:18 +0000

    Commented file.

--- PacketMetrics/bandMean.r
+++ PacketMetrics/bandMean.r
@@ -1,4 +1,14 @@
 #!/usr/bin/env Rscript
+#-----------------------------------------------------
+#-----------------------------------------------------
+#--                Function Name: bandMean          --
+#--                Name: Band Mean                  --
+#--           Input: window - the samples           --
+#--                     a   - lower band            -- 
+#--                     b   - upper band            --
+#--           Output : mean of window               --
+#-----------------------------------------------------
+#-----------------------------------------------------
 bandMean <- function(window,a,b){
 	sum = 0
 	for (i in a:b){
