fdbbfcfa24c9642795b20a85ed811d92a05c142c
James Cox <james.a.cox91@googlemail.com>
2014-02-10 12:22:26 +0000

    Removed bandMean and added to a new script

--- PacketMetrics/bandTDEV.r
+++ PacketMetrics/bandTDEV.r
@@ -1,36 +1,35 @@
 #!/usr/bin/env Rscript
 
+#-----------------------------------------------------
+#-----------------------------------------------------
+#--              Function Name: bandTDEV            --
+#--             Name: band Time Deviation           --
+#--        Input: nTo - position in list            --
+#--                N   - number of samples          -- 
+#--                x   - vector of samples          --
+#--       Output : band time deviation              --
+#-----------------------------------------------------
+source("bandMean.r")
 
-#Input Data 
-
-bandTDEV <- function(nTo,N){
-To <- 0.1
-n <- nTo / To
-test <- c(1,2,3)
-window <- 15
-windowStep <- (window - 1) / 2
-outerStep <- 0
-a <- 20
-b <- 80
-for (j in 1:(N-3*n + 1)){
-	interimStep <- 0
-	for (i in j:(n+j - 1)){
-		interimStep <- interimStep + bandMean(x[i + 2*n - windowStep : i + 2*n + windowStep],a,b) - 2 * bandMean(x[i+n - windowStep : i + n + windowStep],a,b) + bandMean(x[i - windowStep : i + windowStep],a,b) 
+bandTDEV <- function(nTo,N,x){
+	To <- 0.1 # set minimum step
+	n <- nTo / To #number of samples to current
+	window <- 15
+	windowStep <- (window - 1) / 2 #set side of window
+	outerStep <- 0
+	a <- 20
+	b <- 80
+	for (j in 1:(N-3*n + 1)){
+		interimStep <- 0
+		for (i in j:(n+j - 1)){
+			interimStep <- interimStep + bandMean(x[i + 2*n - windowStep : i + 2*n + windowStep],a,b) - 2 * bandMean(x[i+n - windowStep : i + n + windowStep],a,b) + bandMean(x[i - windowStep : i + windowStep],a,b) 
+		}
+		interimStep = interimStep ^ 2
+		outerStep = outerStep + interimStep
 	}
-	interimStep = interimStep ^ 2
-	outerStep = outerStep + interimStep
-}
-outerStep = outerStep / (6 * n^2 * (N - 3*n + 1))
-result <- sqrt(outerStep)
-return(result) 
-}
-bandMean <- function(window,a,b){
-	sum = 0
-	for (i in a:b){
-		sum = sum + window(i)
-	}
-	average = sum / (b - a + 1)
-	return (average)
+	outerStep = outerStep / (6 * n^2 * (N - 3*n + 1))
+	result <- sqrt(outerStep)
+	return(result) 
 }
 
 	
--- PacketMetrics/percentileTDEV.r
+++ PacketMetrics/percentileTDEV.r
@@ -1,39 +1,39 @@
 #!/usr/bin/env Rscript
 
-
-#Input Data 
-
-bandTDEV <- function(nTo,N){
-To <- 0.1
-n <- nTo / To
-test <- c(1,2,3)
-window <- 15
-windowStep <- (window - 1) / 2
-outerStep <- 0
-a <- 0
-b <- 80
-for (j in 1:(N-3*n + 1)){
-	interimStep <- 0
-	for (i in j:(n+j - 1)){
-		interimStep <- interimStep + bandMean(x[i + 2*n - windowStep : i + 2*n + windowStep],0,b) - 2 * bandMean(x[i+n - windowStep : i + n + windowStep],0,b) + bandMean(x[i - windowStep : i + windowStep],0,b) 
+#-----------------------------------------------------
+#-----------------------------------------------------
+#--        Function Name: percentileTDEV            --
+#--       Name: percentile Time Deviation           --
+#--        Input: nTo - position in list            --
+#--                N   - number of samples          -- 
+#--                x   - vector of samples          --
+#--      Output : percentile time deviation         --
+#--       Note: Same as bandTDEV, just a = 0        --
+#-----------------------------------------------------
+#-----------------------------------------------------
+source("bandMean.r")
+percentileTDEV <- function(nTo,N,x){
+	To <- 0.1 #time between samples
+	n <- nTo / To #number of samples to current
+	window <- 15 
+	windowStep <- (window - 1) / 2 #get side of window
+	outerStep <- 0
+	a <- 0
+	b <- 80
+	for (j in 1:(N-3*n + 1)){
+		interimStep <- 0
+		for (i in j:(n+j - 1)){
+			interimStep <- interimStep + bandMean(x[i + 2*n - windowStep : i + 2*n + windowStep],0,b) - 2 * bandMean(x[i+n - windowStep : i + n + windowStep],0,b) + bandMean(x[i - windowStep : i + windowStep],0,b) 
+		}
+		interimStep = interimStep ^ 2
+		outerStep = outerStep + interimStep
 	}
-	interimStep = interimStep ^ 2
-	outerStep = outerStep + interimStep
-}
-outerStep = outerStep / (6 * n^2 * (N - 3*n + 1))
-result <- sqrt(outerStep)
-return(result) 
-}
-bandMean <- function(window,a,b){
-	sum = 0
-	for (i in a:b){
-		sum = sum + window(i)
-	}
-	average = sum / (b - a + 1)
-	return (average)
+	outerStep = outerStep / (6 * n^2 * (N - 3*n + 1))
+	result <- sqrt(outerStep)
+	return(result) 
 }
 
 	
 
-print(bandTDEV(12,12))
+print(percentileTDEV(12,12))
 
