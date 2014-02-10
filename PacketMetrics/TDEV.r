c60dae543f6b8da2e30b8261bbe38a6a5c417760
James Cox <james.a.cox91@googlemail.com>
2014-02-10 11:30:59 +0000

    Added mean function call to the other values

--- PacketMetrics/TDEV.r
+++ PacketMetrics/TDEV.r
@@ -18,7 +18,7 @@ outerStep <- 0
 for (j in 1:(N-3*n + 1)){
 	interimStep <- 0
 	for (i in j:(n+j - 1)){
-		interimStep <- interimStep + mean(x[i + 2*n - windowSide:i + 2*n + windowSide]) - 2* x[i+n - windowSide : i + n + windowSide] + x[i - windowSide : i + windowSide] 
+		interimStep <- interimStep + mean(x[i + 2*n - windowSide:i + 2*n + windowSide]) - 2* mean(x[i+n - windowSide : i + n + windowSide]) + mean(x[i - windowSide : i + windowSide])
 	}
 	interimStep = interimStep ^ 2
 	outerStep = outerStep + interimStep
