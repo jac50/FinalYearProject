216be381ea71ebb0b3fee9a0c001ffa517297a16
James Cox <james.a.cox91@googlemail.com>
2014-02-11 10:47:24 +0000

    Mirrored TDEV, but changed the mean function call to min

--- PacketMetrics/minTDEV.r
+++ PacketMetrics/minTDEV.r
@@ -1,39 +1,31 @@
 #!/usr/bin/env Rscript
 #-----------------------------------------------------
 #-----------------------------------------------------
-#--             Function Name: minTDEV              --
-#--         Name: Minimum Time Deviation            --
-#--         Input: nTo - position in list           --
-#--                 N   - number of samples         -- 
-#--                 x   - vector of samples         --
-#--           Output :min. time deviation           --
+#--                Function Name: minTDEV           --
+#--                Name: Minimum Time Deviation     --
+#--           Input: nTo - position in list         --
+#--                  N   - number of samples        -- 
+#--                  x   - vector of samples        --
+#--           Output : time deviation               --
 #-----------------------------------------------------
 #-----------------------------------------------------
-
-minTDEV <- function(nTo,N,x){
-	To <- 0.1 #time between samples
-	n <- nTo / To #number of samples to current point
-
-	window <- 15 
-	windowSide <- (window - 1) / 2 #Calculate left and right side of window
-	outerStep <- 0
-	for (j in 1:(N-3*n + 1)){
+minTDEV <- function(To,n, N,x){
+#	To <- 0.1 #time between samples
+#	n <- nTo / To #number of samples to current point
+	window <- 5 # Set window Size
+	windowSide <- (window - 1) / 2 # Set the length of Side of window
+	outerStep <- 0 
+	for (j in windowSide+1:(N-3*n + 1) - windowSide){
 		interimStep <- 0
 		for (i in j:(n+j - 1)){
-			# As list is sorted, min value is far left value
-			# in window
-			interimStep <- interimStep + x[i + 2*n - windowSide] - 2* x[i+n - windowSide] + x[i - windowSide] 
+			interimStep <- interimStep + min(x[(i + (2*n)) - windowSide:(i + (2*n)) + windowSide]) - 2* min(x[i+n - windowSide : i + n + windowSide]) + min(x[i - windowSide : i + windowSide])
 		}
 		interimStep = interimStep ^ 2
 		outerStep = outerStep + interimStep
 	}
-	outerStep = outerStep / (6 * (N - 3*n + 1));
+	outerStep = outerStep / (6 * (N - (3*n) + 1));
 	result <- sqrt(outerStep)
 	return(result) 
 }
 
-#--- Test Code --------
-#- Generate samples  --
-x <- seq(0,1000)
-print(minTDEV(12,12,x)) #Test Data
 
