9c5dbf704a547742836ec15fef3d7d79d3aceae4
James Cox <james.a.cox91@googlemail.com>
2014-02-11 10:45:27 +0000

    Added in functionality for minTDEV, and a command line arg for sample size.

--- PacketMetrics/PacketMetric.r
+++ PacketMetrics/PacketMetric.r
@@ -7,11 +7,16 @@
 # ----------------------------------------------------
 
 source("TDEV.r") #Import TDEV Script
-
+source("minTDEV.r") #Imprt MinTDEV Script
 #---------- Import Data into script -----------
+arguments  <- commandArgs()
+sampleSize <- arguments[6] #Command line args start from index 6
+
+fileName = paste("../PTPData/SampleSize_", sampleSize, ".txt",sep="")
+print(fileName)
 
 print ("Reading CSV Data...")
-Data <- read.csv(file = "../PTPData/subsetData.txt",head = TRUE, sep=",")
+Data <- read.csv(file = fileName,head = TRUE, sep=",")
 print ("CSV Data has been written to Data variable")
 delays <- as.matrix(Data[4])
 # delays <-sort(delays) Sort if needed
@@ -20,17 +25,21 @@ To <- 1/16 #Assume To = 1/16
 delays = delays[-1] 
 delays = delays[-1]
 delays = delays[-1]
-print (delays)
-N <- 46
+#print (delays)
+N <- as.numeric(sampleSize) - 4 #1 for the header, 2 for init, and 1 for the null value
+
 maxn = floor(N / 3)
 
 result = matrix(0,maxn)
+resultMinTDEV = matrix(0,maxn)
+
 for (i in 1:maxn){
 	
-	result[i] <- TDEV(To,i,46,delays)
+	#result[i] <- TDEV(To,i,N,delays)
+	resultMinTDEV[i] <-minTDEV(To,i,N,delays)
 }
 
-print(result)
+print(resultMinTDEV)
 
 #Create a CSV output file
 
