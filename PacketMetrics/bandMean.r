00f4ec61be1c33b244f1effcc521e61c53cc87bd
James Cox <james.a.cox91@googlemail.com>
2014-02-13 17:54:30 +0000

    Array access not a function call...

--- PacketMetrics/bandMean.r
+++ PacketMetrics/bandMean.r
@@ -12,8 +12,9 @@
 bandMean <- function(window,a,b){
 	sum = 0
 	for (i in a:b){
-		sum = sum + window(i) # sum window from a to b
+		sum = sum + window[i] # sum window from a to b
 	}
 	average = sum / (b - a + 1)
+	print(window)
 	return (average)
 }
