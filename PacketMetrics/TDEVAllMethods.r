4d529a2c4bb8e217c76c20132d6273c98becc7d9
James Cox <james.a.cox91@googlemail.com>
2014-02-14 13:19:52 +0000

    Changed to use one for loop

--- PacketMetrics/TDEVAllMethods.r
+++ PacketMetrics/TDEVAllMethods.r
@@ -19,15 +19,14 @@ TDEVAll <- function(To,n, N,x,a,b){
 	interimStep <- c(0,0,0,0)
 	outerStep <- c(0,0,0,0)
 	result <- c(0,0,0,0)
-	for (j in windowSide+1:(N-3*n + 1) - windowSide){
+	for (i in windowSide+1:(N-3*n + 1) - windowSide){
 		interimStep <- c(0,0,0,0)	
-		for (i in j:(n+j - 1)){
 			interimStep[1] <- interimStep[1] + mean(x[(i + (2*n)) - windowSide:(i + (2*n)) + windowSide]) - 2* mean(x[i+n - windowSide : i + n + windowSide]) + mean(x[i - windowSide : i + windowSide])
 			
 			interimStep[2] <- interimStep[2] + min(x[(i + (2*n)) - windowSide:(i + (2*n)) + windowSide]) - 2* min(x[i+n - windowSide : i + n + windowSide]) + min(x[i - windowSide : i + windowSide])
 			interimStep[3] <- interimStep[3] + bandMean(x[(i + (2*n)) - windowSide:(i + (2*n)) + windowSide],a,b) - 2* bandMean(x[i+n - windowSide : i + n + windowSide],a,b) + bandMean(x[i - windowSide : i + windowSide],a,b)
 		#	interimStep[4] <- interimStep[4] + bandMean(x[(i + (2*n)) - windowSide:(i + (2*n)) + windowSide],0,b) - 2* bandMean(x[i+n - windowSide : i + n + windowSide],0,b) + bandMean(x[i - windowSide : i + windowSide],0,b)
-		}
+		
 		for (k in 1:4){
 			interimStep[i] <- interimStep[i] ^ 2
 			result[i] <- result[i] + interimStep[i]
