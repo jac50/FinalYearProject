2f9b381cd424b4d9ff5dfc6042c0158e5c666fa0
James Cox <james.a.cox91@googlemail.com>
2014-02-14 13:20:35 +0000

    Created minMATIE script

---
+++ PacketMetrics/minMATIE.r
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+#-----------------------------------------------------
+#-----------------------------------------------------
+#--                Function Name: TDEV              --
+#--                Name: Time Deviation             --
+#--           Input: nTo - position in list         --
+#--                  N   - number of samples        -- 
+#--                  x   - vector of samples        --
+#--           Output : time deviation               --
+#-----------------------------------------------------
+#-----------------------------------------------------
+MATIE <- function(To,n, N,x){
+#	To <- 0.1 #time between samples
+#	n <- nTo / To #number of samples to current point
+	window <- 5 # Set window Size
+	windowSide <- (window - 1) / 2 # Set the length of Side of window
+	outerStep <- 0 
+	result = matrix(0,(N - 2*n + 1))
+	for (i in windowSide + 1:(N-2*n + 1) - windowSide){
+		interimStep <- 0
+		for (j in i:(n+k-1)){
+			interimStep <- interimStep + min(x[i+n - windowSide: i + n + windowSide]) +  min(x[i - windowSide : i + windowSide])
+		}
+		interimStep = abs(interimStep)
+		interimStep = interimStep / n
+		result[i] = interimStep
+	}		
+	return(max(result))
+}
+
+
