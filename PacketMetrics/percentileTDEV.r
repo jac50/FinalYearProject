7361dbda7b4cbcabaf0d97b95e580cfdef401206
James Cox <james.a.cox91@googlemail.com>
2014-02-14 13:19:24 +0000

    One for loop instead of 2

--- PacketMetrics/percentileTDEV.r
+++ PacketMetrics/percentileTDEV.r
@@ -20,11 +20,9 @@ percentileTDEV <- function(nTo,N,x){
 	outerStep <- 0
 	a <- 0
 	b <- 80
-	for (j in 1:(N-3*n + 1)){
+	for (i in 1:(N-3*n + 1)){
 		interimStep <- 0
-		for (i in j:(n+j - 1)){
 			interimStep <- interimStep + bandMean(x[i + 2*n - windowStep : i + 2*n + windowStep],0,b) - 2 * bandMean(x[i+n - windowStep : i + n + windowStep],0,b) + bandMean(x[i - windowStep : i + windowStep],0,b) 
-		}
 		interimStep = interimStep ^ 2
 		outerStep = outerStep + interimStep
 	}
@@ -33,8 +31,4 @@ percentileTDEV <- function(nTo,N,x){
 	return(result) 
 }
 
-#--- Test Code --------
-#- Generate samples  --
-x <- seq(0,1000)
-print(percentileTDEV(12,12,x))
 
