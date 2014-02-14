f377439d9b20a6ce00531c075d11ab4ff2a7b4bd
James Cox <james.a.cox91@googlemail.com>
2014-02-14 12:30:12 +0000

    C file which replaces the TDEV.r function

---
+++ PacketMetrics/TDEV.c
@@ -0,0 +1,44 @@
+//-----------------------------------------------------
+//-----------------------------------------------------
+//--                Function Name: TDEV              --
+//--                Name: Time Deviation             --
+//--           Input: nTo - position in list         --
+//--                  N   - number of samples        -- 
+//--                  x   - vector of samples        --
+//--           Output : time deviation               --
+//-----------------------------------------------------/
+//-----------------------------------------------------
+#include <math.h>
+#include <stdio.h>
+void TDEV(int To, int* n, int* N ,double * x,double * result,int * foo){
+	
+	int window = 5; // Set window Size
+	int windowSide =  (window - 1) / 2; // Set the length of Side of window
+	double outerStep =  0; 
+	
+	double interimStep = 0;
+	double average[3] = {0,0,0};
+	*foo = 7;
+	for (int i=windowSide; i < *N - 3 * (*n) ; i++){
+		interimStep =  0;
+			for (int k=0;k < window;k++){
+				average[0] = average[0] + x[i + 2*(*n) - windowSide + k];
+				average[1] = average[1] + x[i - *n - windowSide + k];
+				average[2] = average[2] + x[i - windowSide + k];
+			}
+			average[0] = average[0] / window;
+			average[1] = average[1] / window;
+			average[2] = average[2] / window;
+			interimStep = average[0] - 2 * average[1] + average[2];
+			
+			//printf("InterimStep: %0.9f", interimStep * interimStep);
+			outerStep = outerStep + (interimStep * interimStep);
+
+			average[0] = average[1] = average[2] = 0;
+	}
+	
+	outerStep = outerStep / (6 * (*N - (3)*(*n) + 1));
+	*result = sqrt(outerStep);
+
+}
+
