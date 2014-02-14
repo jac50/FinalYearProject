d092c47b8d1d4808bc18dcb2f1ab4bfcab8288e9
James Cox <james.a.cox91@googlemail.com>
2014-02-14 12:34:19 +0000

    Changed to one loop

--- PacketMetrics/TDEV.r
+++ PacketMetrics/TDEV.r
@@ -9,17 +9,15 @@
 #--           Output : time deviation               --
 #-----------------------------------------------------
 #-----------------------------------------------------
-TDEV <- function(To,n, N,x){
+TDEVChange <- function(To,n, N,x){
 #	To <- 0.1 #time between samples
 #	n <- nTo / To #number of samples to current point
 	window <- 5 # Set window Size
 	windowSide <- (window - 1) / 2 # Set the length of Side of window
 	outerStep <- 0 
-	for (j in windowSide+1:(N-3*n + 1) - windowSide){
+	for (i in windowSide+1:(N-3*n + 1) - windowSide){
 		interimStep <- 0
-		for (i in j:(n+j - 1)){
-			interimStep <- interimStep + mean(x[(i + (2*n)) - windowSide:(i + (2*n)) + windowSide]) - 2* mean(x[i+n - windowSide : i + n + windowSide]) + mean(x[i - windowSide : i + windowSide])
-		}
+		interimStep <- interimStep + mean(x[(i + (2*n)) - windowSide:(i + (2*n)) + windowSide]) - 2* mean(x[i+n - windowSide : i + n + windowSide]) + mean(x[i - windowSide : i + windowSide])
 		interimStep = interimStep ^ 2
 		outerStep = outerStep + interimStep
 	}
