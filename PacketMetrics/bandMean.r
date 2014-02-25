781ff286c3f3c44461b32202006b1edcf57f3edd
James Cox <james.a.cox91@googlemail.com>
2014-02-25 09:55:03 +0000

    Changed band to be a decimal rather than a percentage

--- PacketMetrics/bandMean.r
+++ PacketMetrics/bandMean.r
@@ -10,10 +10,15 @@
 #-----------------------------------------------------
 #-----------------------------------------------------
 bandMean <- function(window,a,b){
-	sum = 0
+	sum <- 0
+	a <- (a / 100) * length(window)
+	b <- (b / 100) * length(window)
+	a = round(a) + 1 # 1 indexed
+	b = round(b)
+	
 	for (i in a:b){
-		sum = sum + window[i] # sum window from a to b
+		sum <- sum + window[i] # sum window from a to b
 	}
-	average = sum / (b - a + 1)
+	average <- sum / (b - a + 1)
 	return (average)
 }
