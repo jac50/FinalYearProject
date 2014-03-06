d33247ee204c058dd325e7f7ad37c3fa8f867c04
James Cox <james.a.cox91@googlemail.com>
2014-03-06 11:01:44 +0000

    Updated to list available files to rsync. prompts user for input

--- PTPData/TestData/rsyncToBeagleBone.sh
+++ PTPData/TestData/rsyncToBeagleBone.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+echo "RSync List-only will run"
+rsync --list-only jac50@eepc-rjw-beaglebone.bath.ac.uk:/mnt/sd/eesrjw/ ./
 
-rsync -v --progress jac50@eepc-rjw-beaglebone.bath.ac.uk:/home/eesrjw/$1 ./
+echo "Type filename here: "
+read fileName
+
+rsync -v --progress jac50@eepc-rjw-beaglebone.bath.ac.uk:/mnt/sd/eesrjw/$fileName ./NotSorted
+
+
