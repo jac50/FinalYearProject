b7dab439b87b5e3b1ed322d1eec30c899a8ee751
James Cox <james.a.cox91@googlemail.com>
2014-03-10 17:03:08 +0000

    Arguments code removed, added to new file, and tested

--- PacketMetrics/PacketMetric.r
+++ PacketMetrics/PacketMetric.r
@@ -21,33 +21,12 @@ source("MATIEAllMethods.r")
 source("FuncsPacketMetric.r")
 dyn.load("TDEV.so") # C function
 cat("Loaded Required Scripts\n")
-# ------------------------------------------------
-# ------------------------------------------------
-# ----- Initialise All Logging Information -------
-# ------------------------------------------------
-# ------------------------------------------------
-
 
 # ----- Initialises Variables -----
 sampleSize <- 0
 directory <- ""
-# ----- Sets up the Argument Parser and adds the required arguments -----
-parser <- ArgumentParser(description='Perform some Packet Metrics')
-parser$add_argument('-nTest',metavar='00X',type='integer',default= -1,
-			help = 'This is the test number found in the Summary test sheet',dest="nTest")
-parser$add_argument('-nLines',metavar='N',type='integer',dest="sampleSize", default = 0,
-			help = 'How many lines of the test file do you want - First N lines')
-parser$add_argument('-directory',metavar='dir',dest="directory",default="None", help = 'What directory is the test data in? Use a relative directory.')
-parser$add_argument('-metric', metavar='metric', dest="metrics", default="TDEV", help = 'List what metrics you want to add in.')
-parser$add_argument('-delayDir', metavar='direction', dest="direction", default="Master2Slave", help = "What direction of delays do you want to base the metrics off? Master to Slave or Slave to Master")
-parser$add_argument('--AllTestsForSampleSize', metavar='Sample Size', dest='AllSampleSize', default="None", help="Use this flag if you want all tests to run for a set sample size")
-parser$add_argument('--AllSamplesSizesForTest', metavar = 'Test Number', dest='AllinTest', default="None", help = "USe this flag if you want all the sample sizes run for a particular test")
-parser$add_argument('-CSV',dest='CSV',action="store_true")
-parser$add_argument('-latex',dest='latex',action="store_true")
-parser$add_argument('-v', '--verbose', dest='verbose', action="store_true")
-parser$add_argument('-q', '--quiet', dest='quiet', action="store_true")
-#--------------------------------------------------------------------------------------------------
 
+parser <- createArguments()
 # ----- Parses the arguments and checks to see if they are valid -----
 args <- parser$parse_args()
 if ((args$verbose && args$quiet)|| (!args$verbose && !args$quiet)) {
