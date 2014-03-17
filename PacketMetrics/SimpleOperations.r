0aa7f4327c25e3bf3b84b3e900f76bd6b105deff
James Cox <james.a.cox91@googlemail.com>
2014-03-17 16:45:37 +0000

    Histogram and Delay plot now saved into their correct folders

--- PacketMetrics/SimpleOperations.r
+++ PacketMetrics/SimpleOperations.r
@@ -1,11 +1,17 @@
 
-plotHistogram <- function(data,resultSet) { 
-	hist(data)
+plotHistogram <- function(data) { 
+	
+	outputFileName = paste("../PTPData/Plots/HistogramsOfDelays/Histogram of Delays - Test:", args$nTest, " Sample - N", N, " size.eps",sep = "")
+	postscript(outputFileName)
+	hist(data, xlab = "Bins of Delay", ylab="Frequency", main = )
 
 }
 
 
-plotDelay <- function(delay,resultSet) {
+plotDelay <- function(delay) {
+	
+	outputFileName = paste("../PTPData/Plots/PlotOfDelays/Plot of Delays - Test:", args$nTest, " Sample -", N, " size.eps",sep = "")
+	postscript(outputFileName)
 	plot(delay)
 }
 
