a0522cd69666a10a63f88daaed4a31db95e6eb17
James Cox <james.a.cox91@googlemail.com>
2014-03-18 10:51:42 +0000

    Added a template for plotting the colour histogram diagram and added in stats

--- PacketMetrics/SimpleOperations.r
+++ PacketMetrics/SimpleOperations.r
@@ -15,9 +15,32 @@ plotDelay <- function(delay) {
 	plot(delay)
 }
 
-plotCHistogram <- function () { 
-
+plotCHistogram <- function (delay) { 
+	# Work in Progress. Comment out all lines until it is completed
+	#outputFileName = paste("../PTPData/Plots/PlotOfDelays/Colour Histogram of Delays - Test:", args$nTest, " Sample -", N, " size.eps",sep = "")
+	#postscript(outputFileName)
+	colouredDelayArray = matrix(0,6,100) #temp matrix. need to define size
+	#plot(delay)
+	# -- Flatten the histogram into their corresponding colours. 
+	# --- One slice will therefore be one point in time? or one test?
+	
+	counts <- hist(delay, plot = FALSE)$counts
+	#cat(paste("Test: ",counts))
+	for (i in 1:100) colouredDelayArray[,i] <- counts
+	png("simpleIm.png")
+	image(colouredDelayArray, col=rainbow(256))
 
 }
+
+calculateStats <- function(delay) {
+	mean <- mean(delay)
+	median <- median(delay)
+	stddev <- sd(delay)
+	variance <- var(delay)
+		
+	returnValue<- list("mean" = mean, "median" = median, "stddev" = stddev,"variance" = variance)
+	return (returnValue)
+}
+
 
 
