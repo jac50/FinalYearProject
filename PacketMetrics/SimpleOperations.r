06c31b3eaadb147d70d7dfcf44c41b0851c07bb4
James Cox <james.a.cox91@googlemail.com>
2014-03-21 16:18:19 +0000

    Histogram function fleshed out

--- PacketMetrics/SimpleOperations.r
+++ PacketMetrics/SimpleOperations.r
@@ -19,16 +19,23 @@ plotCHistogram <- function (delay) {
 	# Work in Progress. Comment out all lines until it is completed
 	#outputFileName = paste("../PTPData/Plots/PlotOfDelays/Colour Histogram of Delays - Test:", args$nTest, " Sample -", N, " size.eps",sep = "")
 	#postscript(outputFileName)
-	colouredDelayArray = matrix(0,6,100) #temp matrix. need to define size
+	colouredDelayArray = matrix(0,10,20) #temp matrix. need to define size
 	#plot(delay)
 	# -- Flatten the histogram into their corresponding colours. 
 	# --- One slice will therefore be one point in time? or one test?
+	step <- floor(length(delay) / 5) # 100 steps for the time being. 
+	print (step)
+	j = 1
+	problemStep <- length(delay) / step
+	for (i in seq(1, length(delay), by=step))  {
+		if (is.na(delay[(i+step - 1)])) break
+		colouredDelayArray[j,] <-hist(delay[i:i+step - 1],plot = FALSE)$counts
+		j = j + 1
+	}
 	
-	counts <- hist(delay, plot = FALSE)$counts
 	#cat(paste("Test: ",counts))
-	for (i in 1:100) colouredDelayArray[,i] <- counts
 	png("simpleIm.png")
-	image(colouredDelayArray, col=rainbow(256))
+	image(colouredDelayArray, col=colorRampPalette(c("blue","yellow","red"))(256))
 
 }
 
