35b771353b7c723e70618f141f33349315a27412
James Cox <james.a.cox91@googlemail.com>
2014-03-22 17:55:41 +0000

    Commented code

--- OtherScripts/GenerateSampleTextFiles.sh
+++ OtherScripts/GenerateSampleTextFiles.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+#---------------------------------------------------------------------------
+#-                 Script Name: generateSampleTextfiles.sh                 -
+#--- Description: used to create a selection of files from the given data  -
+#---              set. Uses "head" and saves output in form SampleSize_N   -
+#---              where N is the sampleSize                                -
+#---------------------------------------------------------------------------
 echo "Sample Size Script Generator Started.."
 echo "Awk Script to split file into its usable components Started."
 awk 'BEGIN { printf "Time   One Way Delay   Offset From Master, Slave to Master, MAster to Slave" 
 		printf"-----------------------" }
 
 {print $2, $5 $6, $7, $8}' /home/james/FinalYearProject/PTPData/TestData/ExampleData/timeport_to_software_example.txt > Test.txt
+
+# Only takes the 2nd, 5th, 6th, 7th and 8th column of the data file. 
 
 echo "Awk Script Finished"
 head -n 50 Test.txt > SampleSize_50.txt
