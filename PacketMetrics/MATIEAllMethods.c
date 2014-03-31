5fe28f465f3e5a360508ba21b760cf996fde296f
James Cox <james.a.cox91@googlemail.com>
2014-03-31 17:09:17 +0100

    Works fully

--- PacketMetrics/MATIEAllMethods.c
+++ PacketMetrics/MATIEAllMethods.c
@@ -19,7 +19,7 @@ void MATIEAllMethods(double *To, int* n, int* N ,double * x,double * tempResultM
 	double interimStep[2][*N - (2 * (*n)) + 10];
 	double average[2] = {0.0,0.0};
 	for (int i=0; i <= (*N -(2 * (*n)))  + 1 -1; i++){
- 		for (int j=i + windowSide; j < *n + i - 1 - 1 ; j++) { // - 1 as normal. - 1 for index difference
+ 		for (int j=i; j < *n + i - 1 - 1 ; j++) { // - 1 as normal. - 1 for index difference
 			for (int k=0;k < window ;k++){
 				average[0] = average[0] + x[j + (*n) - windowSide + k - 1];
 				average[1] = average[1] + x[j - windowSide + k - 1];
@@ -32,11 +32,8 @@ void MATIEAllMethods(double *To, int* n, int* N ,double * x,double * tempResultM
 			average[j] = average[j] / window;
 		}
 		
-		interimStep[0][i] = abs(average[0] - average[1]) / *n;
-		interimStep[1][i] = abs(minimum[0] - minimum[1]) / *n;
-		for (int k = 0; k < 2; k++) interimStep[k][i] = abs(interimStep[k][i]) / *n;
-		
-		Rprintf("%.9lf\n", interimStep[0][i]);
+		interimStep[0][i] = (average[0] - average[1]) < 0 ? -((average[0] - average[1]) / *n) : ((average[0] - average[1]) / *n);
+		interimStep[1][i] = (minimum[0] - minimum[1]) < 0 ? -(minimum[0] - minimum[1]) / *n : (minimum[0] - minimum[1]) / *n;
 		average[0] = average[1] = 0;
 		minimum[0] = minimum[1] = 10000.00;
 		if (interimStep[0][i] > *tempResultMATIE) *tempResultMATIE = interimStep[0][i];
