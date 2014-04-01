da203749b4c786b083b951968de77a8e23f907ca
James Cox <james.a.cox91@googlemail.com>
2014-04-01 17:52:23 +0100

    Changed CHistogram. Still not fully working.

--- PacketMetrics/SimpleOperations.r
+++ PacketMetrics/SimpleOperations.r
@@ -19,23 +19,30 @@ plotCHistogram <- function (delay) {
 	# Work in Progress. Comment out all lines until it is completed
 	#outputFileName = paste("../PTPData/Plots/PlotOfDelays/Colour Histogram of Delays - Test:", args$nTest, " Sample -", N, " size.eps",sep = "")
 	#postscript(outputFileName)
-	colouredDelayArray = matrix(0,10,20) #temp matrix. need to define size
-	#plot(delay)
+	#logDelay <- log(abs(delay))
+	postscript("Test.eps")
+	logDelay <- delay[2000:length(delay)]
+	plot(delay[2000:length(delay)])
+	step <- 32
+	j <- 0
 	# -- Flatten the histogram into their corresponding colours. 
-	# --- One slice will therefore be one point in time? or one test?
-	step <- floor(length(delay) / 5) # 100 steps for the time being. 
-	print (step)
-	j = 1
-	problemStep <- length(delay) / step
-	for (i in seq(1, length(delay), by=step))  {
-		if (is.na(delay[(i+step - 1)])) break
-		colouredDelayArray[j,] <-hist(delay[i:i+step - 1],plot = FALSE)$counts
+	nStep <- floor(length(logDelay) / step ) # 100 steps for the time being. 
+	colouredDelayArray = matrix(0,nStep,20) #temp matrix. need to define size
+	print(nStep)
+	print(min(logDelay))
+	print(max(logDelay))
+	bins <- seq(0.00000, 0.00010, by = 0.00005)
+	
+	for (i in seq(1, length(logDelay), by = step))  {
+		if (is.na(logDelay[(i+step - 1)])) break
+		
+		colouredDelayArray[j,] <-hist(logDelay[i:i+step - 1],plot = FALSE, breaks = bins)$counts
 		j = j + 1
 	}
-	
+	print(colouredDelayArray)	
 	#cat(paste("Test: ",counts))
 	png("simpleIm.png")
-	image(colouredDelayArray, col=colorRampPalette(c("blue","yellow","red"))(256))
+	image(colouredDelayArray, col=rainbow(256))
 
 }
 
@@ -44,7 +51,9 @@ calculateStats <- function(delay) {
 	median <- median(delay)
 	stddev <- sd(delay)
 	variance <- var(delay)
-		
+	minimum <- min(delay)
+	maximum <- max(delay)
+	
 	returnValue<- list("mean" = mean, "median" = median, "stddev" = stddev,"variance" = variance)
 	return (returnValue)
 }
